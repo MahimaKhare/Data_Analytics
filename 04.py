040728c9fb1f9dd1d221c0c989f87dc02d934084
Mahima Khare <mkhare501@gmail.com>
2025-11-16 22:07:48 +0530

    task_5

---
+++ 04.py
@@ -0,0 +1,95 @@
+# Replace()
+x = "Hillo"
+print(x.replace('i','e'))
+
+# String Format
+fname = "Rahul"
+x1 = f"My name is {fname}"
+print(x1)
+
+#Q.01 
+x = input("enter your name")
+y = int(input("enter your age"))
+x2 = f"My Name is {x} and My age is {y}"
+print(x2)
+
+# Escape Character 
+"""y1 = "Hello\"world"
+print(y1)"""
+
+# Python Booleans 
+print(4>5)
+print(4<5)
+print(4==5)
+print(4!=5)
+
+# Operators
+
+# Airthmatic Operators
+a = 10
+b = 5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+print(a//b) 
+
+#Q.02 Check the number is even or odd
+y2 =int(input("eneter a number"))
+y3 = y2 % 2
+print(y3 == 0)
+
+# Comparision Operators
+a1 = 20
+b1 = 30
+print(a1 == b1)
+print(a1 < b1)
+print(a1 > b1)
+print(a1 <= b1)
+print(a1 >= b1)
+print(a1 != b1)
+
+# Assignment Operator
+a1 = 10
+a1 += 5
+print(a1)
+a1 -= 4
+print(a1)
+a1 *= 3
+print(a1)
+a1 /= 2
+print(a1)
+a1 %= 7
+print(a1)
+a1 **= 4
+print(a1)
+
+#Logical Operator
+i = 20
+j = 30
+# and operator
+print(i<=20 and j==i )
+print(i==20 and j==30)
+# or operator
+print(i<=20 or j==i )
+print(i==20 or j==30)
+# not operator 
+print(not(i==j))
+
+# identifier
+i1 = "Hello"
+j1 = "Hello, How are you"
+# in operator
+print(i1 in j1)
+# not in operator
+print(i1 not in j1)
+
+
+
+
+
+
+
+
