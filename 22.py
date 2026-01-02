3902fa7ea2116c5a4f0423abb86179081e79961e
Mahima Khare <mkhare501@gmail.com>
2026-01-02 13:23:22 +0530

    day 23

--- 22.py
+++ 22.py
@@ -25,45 +25,45 @@ plt.show()
 #01.
 a = df['Age']
 b = df['Sex']
-a1 = np.array([a])
-b1 = np.array([b])
+a1 = np.array(a)
+b1 = np.array(b)
 plt.plot(a1,b1,'o')
 plt.show()
 
 #02.
 x1 = df['Chest pain type']
 x2 = df['BP']
-arr1 = np.array([x1])
-arr2 = np.array([x2])
+arr1 = np.array(x1)
+arr2 = np.array(x2)
 plt.plot(arr1,arr2,'o')
 plt.show()
 
 #03.
-y1 = np.array([df['Cholesterol']])
-y2 = np.array([df['FBS over 120']])
+y1 = np.array(df['Cholesterol'])
+y2 = np.array(df['FBS over 120'])
 plt.plot(y1,y2,'o')
 plt.show()
 
 #04.
-a2 = np.array([df['EKG results']])
-b2 = np.array([df['Max HR']])
+a2 = np.array(df['EKG results'])
+b2 = np.array(df['Max HR'])
 plt.plot(a2,b2,'o')
 plt.show()
 
 #05.
-e = np.array([df['Exercise angina']])
-p = np.array([df['ST depression']])
+e = np.array(df['Exercise angina'])
+p = np.array(df['ST depression'])
 plt.plot(e,p,'o')
 plt.show()
 
 #06.
-e1 = np.array([df['ST depression']])
-e2 = np.array([df['Slope of ST']])
+e1 = np.array(df['ST depression'])
+e2 = np.array(df['Slope of ST'])
 plt.plot(e1,e2,'o')
 plt.show()
 
 #07.
-f1 = np.array([df['Number of vessels fluro']])
-f2 = np.array([df['Thallium']])
+f1 = np.array(df['Number of vessels fluro'])
+f2 = np.array(df['Thallium'])
 plt.plot(f1,f2,'o')
 plt.show()
---
+++ 23.py
@@ -0,0 +1,155 @@
+import matplotlib.pyplot as plt
+import numpy as np
+x = np.array([0,2,4,6])
+y = np.array([8,10,12,14])
+
+#Markers
+plt.plot(x,y,marker = 'o')
+plt.show()
+
+# Star
+plt.plot(x,y,marker = '*')
+plt.show()
+
+#point
+plt.plot(x,y, marker = '.')
+plt.show()
+
+#pixel
+plt.plot(x,y, marker = ',')
+plt.show()
+
+#cross
+plt.plot(x,y, marker = 'x')
+plt.show()
+
+#x filled
+plt.plot(x,y, marker = 'X')
+plt.show()
+
+#plus
+plt.plot(x,y, marker = '+')
+plt.show()
+
+#plus filled
+plt.plot(x,y, marker = 'P')
+plt.show()
+
+#Hexagon
+plt.plot(x,y, marker = 'H')
+plt.show()
+
+# Hexagon
+plt.plot(x,y, marker = 'h')
+plt.show()
+
+#Square
+plt.plot(x,y, marker = 's')
+plt.show()
+
+#
+#point
+plt.plot(x,y, marker = '.')
+plt.show()
+
+#pixel
+plt.plot(x,y, marker = ',')
+plt.show()
+
+#cross
+plt.plot(x,y, marker = 'x')
+plt.show()
+
+#Dimond
+plt.plot(x,y, marker = 'd')
+plt.show()
+
+#Dimond Filled
+plt.plot(x,y, marker = 'D')
+plt.show()
+
+#pentagon
+plt.plot(x,y, marker = 'P')
+plt.show()
+
+#Triangle Down
+plt.plot(x,y, marker = '^')
+plt.show()
+
+#Triangle Up
+plt.plot(x,y, marker = 'v')
+plt.show()
+
+#Triangle left
+plt.plot(x,y, marker = '<')
+plt.show()
+
+#Triangle right
+plt.plot(x,y, marker = '>')
+plt.show()
+
+#Triangle Down
+plt.plot(x,y, marker = '1')
+plt.show()
+
+#Triangle Up
+plt.plot(x,y, marker = '2')
+plt.show()
+
+#Triangle left
+plt.plot(x,y, marker = '3')
+plt.show()
+
+#Triangle right
+plt.plot(x,y, marker = '4')
+plt.show()
+
+# vartical line
+plt.plot(x,y, marker = '|')
+plt.show()
+
+#Format string
+plt.plot(x,y, 'o:y')
+plt.show()
+
+#solid line
+plt.plot(x,y, '*-r')
+plt.show()
+
+#dashed line
+plt.plot(x,y, '.--b')
+plt.show()
+
+#dashed dotted
+plt.plot(x,y, '+-.g')
+plt.show()
+
+#Increasing marker size
+plt.plot(x,y, marker = '*', ms = 20)
+plt.show()
+
+# marker border color
+plt.plot(x,y, marker = '*', ms = 20, mec = 'r')
+plt.show()
+
+#marker face color
+plt.plot(x,y, marker = '*', ms = 20, mec = 'r', mfc = 'b')
+plt.show()
+
+#line widht
+plt.plot(x,y, '.--b', lw = '20')
+plt.show()
+
+# line style 
+plt.plot(x,y, '.--b', ls = 'solid')
+plt.show()
+
+plt.plot(x,y, '.--b', ls = '-')
+plt.show()
+
+plt.plot(x,y, '.--b', ls = '-.')
+plt.show()
+
+#Line color 
+plt.plot(x,y, '.--b', ls = '-', color = 'r')
+plt.show()
