6c59c624b0bc2fcde9c4d793fcd860fc0bdfa45d
Mahima Khare <mkhare501@gmail.com>
2026-01-21 22:50:11 +0530

    SQL

---
+++ Sql/02.sql
@@ -0,0 +1,45 @@
+Create Database Restaurant;
+Use Restaurant;
+
+CREATE TABLE person (
+    PID INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Address VARCHAR(100)
+);
+
+-- Alter
+-- 01. Add coloumn 
+Alter Table person
+Add last_name varchar(50);
+
+-- 02. Modify
+Alter Table person
+add last_name char;  
+
+-- 03. Rename
+Alter Table person
+rename column last_name to l_name;  
+
+-- 04. drop
+Alter Table person 
+Drop l_name;
+
+-- Constraints
+use restaurant;
+
+Create Table orders( 
+    OID INT PRIMARY KEY,
+    OrderName VARCHAR(50) not null,
+    PID INT,
+    FOREIGN KEY(PID) REFERENCES person(PID)
+);
+
+Select * from person;
+Select * from orders;
+
+
+
+
+
+   
+    
